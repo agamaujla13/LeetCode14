c2c4e18f088e8ac5aea6f1d46dc4716c2adbc439
AGAM AUJLA <agamaujla14@gmail.com>
2026-07-28 14:03:05 +0530

    Time: 3 ms (52.53%) | Memory: 14.2 MB (46.92%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> curr;
+
+    void solve(vector<int>& candidates, int target, int index) {
+        if (target == 0) {
+            ans.push_back(curr);
+            return;
+        }
+
+        if (index == candidates.size() || target < 0)
+            return;
+
+        // Take current element
+        curr.push_back(candidates[index]);
+        solve(candidates, target - candidates[index], index);
+        curr.pop_back();
+
+        // Skip current element
+        solve(candidates, target, index + 1);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        solve(candidates, target, 0);
+        return ans;
+    }
+};
